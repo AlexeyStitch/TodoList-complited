778e77e9542df9d4a1de3e2130e53a7570bf3dfc
AlexeyStitcg <ljrevtyn12!>
2020-06-19 17:00:05 +0500

    todo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoList from "./TodoList";
+import CreateForm from "./CreateForm";
+import userEvent from "@testing-library/user-event";
+
+const initialTodos = [
+  {id: 1, name: 'Test1', done: false},
+  {id: 2, name: 'Test2', done: false},
+]
 
 function App() {
+
+  const [todos, setTodos] = useState(initialTodos);
+
+  const onCreateTask = (task) => {
+    console.log(task + 'AppCreate')
+    const updateOnCreateTask = [...todos];
+    updateOnCreateTask.push({id: Math.random(), name: task, done: false})
+    setTodos(updateOnCreateTask)
+  }
+
+  const onDoneTask = (id) => {
+    const updateOnDoneTask = todos.map(el => {
+      (el.done === el.id) return {...el, done: true}
+      else return el;
+    })
+    setTodos(updateOnDoneTask)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<TodoList todos={todos} onDoneTask={onDoneTask}/>
+<CreateForm onCreateTask={onCreateTask}/>
+
     </div>
   );
 }
